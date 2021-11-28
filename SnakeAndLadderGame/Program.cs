4b0f5a6b691f89c802266b12a138bb1a6945f9b4
Wpaswan <dablumcapaswan@gmail.com>
2021-11-28 02:07:49 -0800

    Player position program added

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -3,9 +3,10 @@ Console.WriteLine("Please enter any key to start and n to stop");
 char c = Convert.ToChar(Console.ReadLine());
 while (c != 'n')
 {
-    Console.WriteLine("\t((((((((Please Selec Any))))))))");
-    Console.WriteLine("\t((((((((1.Start With Zero))))))))");
-    Console.WriteLine("\t((((((((2.Roll Dice      ))))))))");
+    Console.WriteLine("\t((((((((Please Selec Any          ))))))))");
+    Console.WriteLine("\t((((((((1.Start With Zero         ))))))))");
+    Console.WriteLine("\t((((((((2.Roll Dice               ))))))))");
+    Console.WriteLine("\t((((((((3.Position of player      ))))))))");
     int ch = Convert.ToInt32(Console.ReadLine());
     switch (ch)
     {
@@ -16,6 +17,10 @@ while (c != 'n')
             case 2:
             RollDice rd=new RollDice();
             rd.Dice_roll();
+            break;
+        case 3:
+            PlayerPosition pp=new PlayerPosition();
+            pp.position();
             break;
 
           
