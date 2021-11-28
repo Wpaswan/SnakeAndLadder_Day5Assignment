70d1412533f5f709531da09a09f0e7a9d51525d1
Wpaswan <dablumcapaswan@gmail.com>
2021-11-28 01:57:46 -0800

    Roll Dice added

--- SnakeAndLadderGame/Program.cs
+++ SnakeAndLadderGame/Program.cs
@@ -5,6 +5,7 @@ while (c != 'n')
 {
     Console.WriteLine("\t((((((((Please Selec Any))))))))");
     Console.WriteLine("\t((((((((1.Start With Zero))))))))");
+    Console.WriteLine("\t((((((((2.Roll Dice      ))))))))");
     int ch = Convert.ToInt32(Console.ReadLine());
     switch (ch)
     {
@@ -12,6 +13,11 @@ while (c != 'n')
             StartPositionZero sp = new StartPositionZero();
             sp.Startzero();
             break;
+            case 2:
+            RollDice rd=new RollDice();
+            rd.Dice_roll();
+            break;
+
           
     }
     Console.WriteLine("Please enter any key to start and n to stop");
---
+++ SnakeAndLadderGame/RollDice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    internal class RollDice
+    {
+        public void Dice_roll()
+        {
+            Console.WriteLine("Welcome to snake ladder Game");
+            int Start_Position = 0;
+            Random random = new Random();
+
+            int Dice_No = random.Next(1, 7);
+            Console.WriteLine("Player roll dice and dice no is :  " + "   " +Dice_No);
+        }
+    }
+}
